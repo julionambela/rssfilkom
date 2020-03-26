8a04863cf10b18b75bc5bcbb17189708a45caacc
Julio <julionambela@gmail.com>
2020-03-26 13:13:46 +0700

    Ngilangin event di webhook

--- src/main/java/com/summer/rssfilkom/demo/Controller.java
+++ src/main/java/com/summer/rssfilkom/demo/Controller.java
@@ -3,23 +3,33 @@ package com.summer.rssfilkom.demo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.client.LineSignatureValidator;
+import com.linecorp.bot.client.MessageContentResponse;
 import com.linecorp.bot.model.Multicast;
 import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.message.*;
+import com.linecorp.bot.model.event.source.GroupSource;
+import com.linecorp.bot.model.event.source.RoomSource;
+import com.linecorp.bot.model.message.FlexMessage;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.message.flex.container.FlexContainer;
 import com.linecorp.bot.model.objectmapper.ModelObjectMapper;
 import com.linecorp.bot.model.profile.UserProfileResponse;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
@@ -39,31 +49,41 @@ public class Controller {
             @RequestBody String eventsPayload)
     {
         try {
-//            if (!lineSignatureValidator.validateSignature(eventsPayload.getBytes(), xLineSignature)) {
-//                throw new RuntimeException("Invalid Signature Validation");
-//            }
+            if (!lineSignatureValidator.validateSignature(eventsPayload.getBytes(), xLineSignature)) {
+                throw new RuntimeException("Invalid Signature Validation");
+            }
 
             // parsing event
             ObjectMapper objectMapper = ModelObjectMapper.createNewObjectMapper();
             EventsModel eventsModel = objectMapper.readValue(eventsPayload, EventsModel.class);
 
-//            eventsModel.getEvents().forEach((event)->{
+            eventsModel.getEvents().forEach((event)->{
+                if (event instanceof MessageEvent) {
+                    if (event.getSource() instanceof GroupSource || event.getSource() instanceof RoomSource) {
+                        handleGroupRoomChats((MessageEvent) event);
+                    } else {
+                        handleOneOnOneChats((MessageEvent) event);
+                    }
+                }
+            });
+
+            eventsModel.getEvents().forEach((event)->{
+                if (event instanceof MessageEvent) {
+                    MessageEvent messageEvent = (MessageEvent) event;
+                    TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
+                    replyText(messageEvent.getReplyToken(), textMessageContent.getText());
+                }
 //                if (event instanceof MessageEvent) {
-//                    MessageEvent messageEvent = (MessageEvent) event;
-//                    TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
-//                    replyText(messageEvent.getReplyToken(), textMessageContent.getText());
+//                MessageEvent messageEvent = (MessageEvent) event;
+//                TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
+//                replyText(messageEvent.getReplyToken(), textMessageContent.getText());
+//                List<Message> msgArray = new ArrayList<>();
+//                msgArray.add(new TextMessage(textMessageContent.getText()));
+//                msgArray.add(new StickerMessage("1", "106"));
+//                ReplyMessage replyMessage = new ReplyMessage(((MessageEvent) event).getReplyToken(), msgArray);
+//                reply(replyMessage);
 //                }
-////                if (event instanceof MessageEvent) {
-////                MessageEvent messageEvent = (MessageEvent) event;
-////                TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
-////                replyText(messageEvent.getReplyToken(), textMessageContent.getText());
-////                List<Message> msgArray = new ArrayList<>();
-////                msgArray.add(new TextMessage(textMessageContent.getText()));
-////                msgArray.add(new StickerMessage("1", "106"));
-////                ReplyMessage replyMessage = new ReplyMessage(((MessageEvent) event).getReplyToken(), msgArray);
-////                reply(replyMessage);
-////                }
-//            });
+            });
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (IOException e) {
             e.printStackTrace();
@@ -116,7 +136,25 @@ public class Controller {
         return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
     }
 
+    @RequestMapping(value = "/content/{id}", method = RequestMethod.GET)
+    public ResponseEntity content(
+            @PathVariable("id") String messageId
+    ){
+        MessageContentResponse messageContent = getContent(messageId);
 
+        if(messageContent != null) {
+            HttpHeaders headers = new HttpHeaders();
+            String[] mimeType = messageContent.getMimeType().split("/");
+            headers.setContentType(new MediaType(mimeType[0], mimeType[1]));
+
+            InputStream inputStream = messageContent.getStream();
+            InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+
+            return new ResponseEntity<>(inputStreamResource, headers, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
     private void reply(ReplyMessage replyMessage) {
         try {
@@ -162,6 +200,74 @@ public class Controller {
             return lineMessagingClient.getProfile(userId).get();
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private MessageContentResponse getContent(String messageId) {
+        try {
+            return lineMessagingClient.getMessageContent(messageId).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void handleOneOnOneChats(MessageEvent event) {
+        if  (event.getMessage() instanceof AudioMessageContent
+                || event.getMessage() instanceof ImageMessageContent
+                || event.getMessage() instanceof VideoMessageContent
+                || event.getMessage() instanceof FileMessageContent
+        ) {
+            handleContentMessage(event);
+        } else if(event.getMessage() instanceof TextMessageContent) {
+            handleTextMessage(event);
+        } else {
+            replyText(event.getReplyToken(), "Unknown Message");
+        }
+    }
+
+    private void handleContentMessage(MessageEvent event) {
+        String baseURL     = "https://namaaplikasianda.herokuapp.com";
+        String contentURL  = baseURL+"/content/"+ event.getMessage().getId();
+        String contentType = event.getMessage().getClass().getSimpleName();
+        String textMsg     = contentType.substring(0, contentType.length() -14)
+                + " yang kamu kirim bisa diakses dari link:\n "
+                + contentURL;
+
+        replyText(event.getReplyToken(), textMsg);
+    }
+
+    private void handleTextMessage(MessageEvent event) {
+        TextMessageContent textMessageContent = (TextMessageContent) event.getMessage();
+
+        if (textMessageContent.getText().toLowerCase().contains("flex")) {
+            replyFlexMessage(event.getReplyToken());
+        } else {
+            replyText(event.getReplyToken(), textMessageContent.getText());
+        }
+    }
+
+    private void replyFlexMessage(String replyToken) {
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            String flexTemplate = IOUtils.toString(classLoader.getResourceAsStream("flex_message.json"));
+
+            ObjectMapper objectMapper = ModelObjectMapper.createNewObjectMapper();
+            FlexContainer flexContainer = objectMapper.readValue(flexTemplate, FlexContainer.class);
+
+            ReplyMessage replyMessage = new ReplyMessage(replyToken, new FlexMessage("Dicoding Academy", flexContainer));
+            reply(replyMessage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void handleGroupRoomChats(MessageEvent event) {
+        if(!event.getSource().getUserId().isEmpty()) {
+            String userId = event.getSource().getUserId();
+            UserProfileResponse profile = getProfile(userId);
+            replyText(event.getReplyToken(), "Hello, " + profile.getDisplayName());
+        } else {
+            replyText(event.getReplyToken(), "Hello, what is your name?");
         }
     }
 
